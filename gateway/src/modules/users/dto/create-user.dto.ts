4098775dc1e0107c8358f643088deadc32ba81ff
AliDeWeb <AliMoradi0Business@gmail.com>
2025-06-06 20:27:04 +0330

    feat: integrate Swagger for API documentation and update user DTO with Swagger decorators

--- gateway/src/modules/users/dto/create-user.dto.ts
+++ gateway/src/modules/users/dto/create-user.dto.ts
@@ -1,13 +1,17 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsOptional, IsString } from 'class-validator';
 
 export class CreateUserDto {
   @IsString()
+  @ApiProperty()
   email: string;
 
   @IsString()
+  @ApiProperty()
   password: string;
 
   @IsString()
   @IsOptional()
+  @ApiProperty({ required: false })
   name?: string;
 }
