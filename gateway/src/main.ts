7b274aa38c066fdcdba49685d23a039fdb071537
AliDeWeb <AliMoradi0Business@gmail.com>
2025-06-06 20:33:28 +0330

    feat: update environment configuration and enhance CORS setup in main application

--- gateway/src/main.ts
+++ gateway/src/main.ts
@@ -2,9 +2,18 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import type { NestExpressApplication } from '@nestjs/platform-express';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+
+  const configService = app.get(ConfigService);
+
+  app.enableCors({
+    origin: configService.get<string>('CLIENTS')?.split(','),
+    credentials: true,
+  });
 
   app.useGlobalPipes(
     new ValidationPipe({
