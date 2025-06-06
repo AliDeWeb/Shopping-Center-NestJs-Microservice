4bec7b93b8c5355fc1377c121d387ba2e07f8b65
AliDeWeb <AliMoradi0Business@gmail.com>
2025-06-06 19:24:36 +0330

    feat: add DB_POSTGRES_HOST to environment configuration

--- users/src/app.module.ts
+++ users/src/app.module.ts
@@ -1,5 +1,6 @@
-import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
+import { Global, Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
   imports: [
@@ -7,6 +8,21 @@ import { ConfigModule } from '@nestjs/config';
       envFilePath: '.env',
       isGlobal: true,
     }),
+
+    TypeOrmModule.forRootAsync({
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        host: configService.get<string>('DB_POSTGRES_HOST'),
+        port: +configService.get<string>('DB_POSTGRES_PORT')!,
+        username: configService.get<string>('DB_POSTGRES_USER'),
+        password: configService.get<string>('DB_POSTGRES_PASSWORD'),
+        database: configService.get<string>('DB_POSTGRES_DB'),
+        autoLoadEntities: true,
+        synchronize: configService.get<string>('NODE_ENV') !== 'production',
+      }),
+      inject: [ConfigService],
+    }),
   ],
 })
+@Global()
 export class AppModule {}
